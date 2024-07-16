e15efd435075f63deb154748525deb20e0952072
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-16 12:20:46 +0200

    Revert TS test

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,5 @@
 const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
-type Test = {
-  id: number
-}
-
 module.exports = createRequestHandler({
-  build: require('path').join(__dirname, '../dist/server') as Test,
+  build: require('path').join(__dirname, '../dist/server'),
 });

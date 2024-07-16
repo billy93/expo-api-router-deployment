993f7bf24f61a50a768ae829adecf3d8cbadaf2f
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-16 11:27:01 +0200

    Change to esm

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
-module.exports = createRequestHandler({
+export default createRequestHandler({
   build: join(import.meta.dirname, '../dist/server'),
   // mode: process.env.NODE_ENV,
 });

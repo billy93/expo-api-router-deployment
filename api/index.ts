c6e2e969a4a182dd6f2145505107fcd02a0f5a2e
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-16 16:29:04 +0200

    Change index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
-import { join } from 'node:path';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { createRequestHandler } from '@expo/server/adapter/vercel';
 
 export default createRequestHandler({
-  build: join(import.meta.dirname, '../dist/server'),
+  build: join(dirname(fileURLToPath(import.meta.url)), '../dist/server'),
 });

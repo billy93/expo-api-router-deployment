dbcca3e8fa805907551d4a859c5e7674e27d69d7
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-16 12:14:28 +0200

    Test

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,9 @@
 const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
+type Test = {
+  id: number
+}
+
 module.exports = createRequestHandler({
   build: require('path').join(__dirname, '../dist/server'),
   mode: process.env.NODE_ENV,

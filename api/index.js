8097bbdb7c5136339126b445528d2a59fac913e5
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-16 11:41:08 +0200

    Fix

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
-import { join } from 'node:path';
-import { createRequestHandler } from '@expo/server/adapter/vercel.js';
+const { createRequestHandler } = require('@expo/server/adapter/vercel');
 
-export default createRequestHandler({
-  build: join(import.meta.dirname, '../dist/server'),
-  // mode: process.env.NODE_ENV,
+module.exports = createRequestHandler({
+  build: require('path').join(__dirname, '../dist/server'),
+  mode: process.env.NODE_ENV,
 });

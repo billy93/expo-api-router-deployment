0be5131598e8330f92a136dc2c6dcfdc2a3b2b54
Lukas Prochazka <prochazka.lu@gmail.com>
2024-07-16 17:03:12 +0200

    Update import .js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { createRequestHandler } from '@expo/server/adapter/vercel';
+import { createRequestHandler } from '@expo/server/adapter/vercel.js';
 
 export default createRequestHandler({
   build: join(dirname(fileURLToPath(import.meta.url)), '../dist/server'),
